d4837a54b64857fb07e1d8218c2ba68f8566d939
Thomas Kieslich <thomas.kieslich@pluswerk.ag>
2026-04-01 11:34:37 +0200

    Update SsoLoginProvider

--- Classes/LoginProvider/SsoLoginProvider.php
+++ Classes/LoginProvider/SsoLoginProvider.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Davitec\DvSsoAuth\LoginProvider;
 
+use Davitec\DvSsoAuth\Configuration\ExtensionSettings;
 use Davitec\DvSsoAuth\Configuration\ExtensionSettingsFactory;
-use TYPO3\CMS\Backend\Controller\LoginController;
 use TYPO3\CMS\Backend\LoginProvider\LoginProviderInterface;
-use TYPO3\CMS\Core\Page\PageRenderer;
+use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Fluid\View\StandaloneView;
+use TYPO3\CMS\Core\View\ViewInterface;
 
 final class SsoLoginProvider implements LoginProviderInterface
 {
@@ -17,25 +17,53 @@ final class SsoLoginProvider implements LoginProviderInterface
     {
     }
 
-    public function render(StandaloneView $view, PageRenderer $pageRenderer, LoginController $loginController): void
+    /**
+     * TYPO3 12/13 login provider API.
+     *
+     * @param mixed $view
+     */
+    public function render($view, $pageRenderer, $loginController): void
     {
         $settings = $this->settingsFactory->createFromExtensionConfiguration('dv_sso_auth');
+        $templatePath = $this->resolveTemplatePath($settings);
+        $ssoLoginUri = $this->buildSsoLoginUri($settings);
 
-        $templatePath = $settings->typo3LoginTemplate;
-        if ($templatePath === '') {
-            $templatePath = 'EXT:dv_sso_auth/Resources/Private/Templates/BackendLogin/SsoLogin.html';
+        if (\is_object($view) && method_exists($view, 'setTemplatePathAndFilename')) {
+            $view->setTemplatePathAndFilename($templatePath);
         }
+        if (\is_object($view) && method_exists($view, 'assign')) {
+            $view->assign('ssoLoginUri', $ssoLoginUri);
+        }
+    }
 
-        $view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName($templatePath));
+    /**
+     * TYPO3 14+ login provider API.
+     */
+    public function modifyView(ServerRequestInterface $request, ViewInterface $view): string
+    {
+        $settings = $this->settingsFactory->createFromExtensionConfiguration('dv_sso_auth');
+        $templatePath = $this->resolveTemplatePath($settings);
+        $view->assign('ssoLoginUri', $this->buildSsoLoginUri($settings));
+        return $templatePath;
+    }
 
+    private function buildSsoLoginUri(ExtensionSettings $settings): string
+    {
         $target = GeneralUtility::getIndpEnv('TYPO3_REQUEST_HOST') . '/typo3/?login_status=login';
         if ($settings->forceSSL && !GeneralUtility::getIndpEnv('TYPO3_SSL')) {
             $target = str_ireplace('http:', 'https:', $target);
         }
 
         $queryStringSeparator = str_contains($settings->loginHandler, '?') ? '&' : '?';
-        $ssoLoginUri = $settings->loginHandler . $queryStringSeparator . 'target=' . rawurlencode($target);
+        return $settings->loginHandler . $queryStringSeparator . 'target=' . rawurlencode($target);
+    }
 
-        $view->assign('ssoLoginUri', $ssoLoginUri);
+    private function resolveTemplatePath(ExtensionSettings $settings): string
+    {
+        $templatePath = $settings->typo3LoginTemplate;
+        if ($templatePath === '') {
+            $templatePath = 'EXT:dv_sso_auth/Resources/Private/Templates/BackendLogin/SsoLogin.html';
+        }
+        return GeneralUtility::getFileAbsFileName($templatePath);
     }
 }
